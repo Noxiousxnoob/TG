c2f8f9e2f88b3cc4d95374fdf19fe5acf2748bad
Noxiousxnoob <154877712+Noxiousxnoob@users.noreply.github.com>
2024-12-04 10:23:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,20 +5,21 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "24269862")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "5b1a646f8c8ed40f15af84c9b2dfa9e8") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7762669342:AAFSMNMxx2HPNn7l6z2e-EuO_smwwIP33M0") # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', 'NoxBots') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
     # database config
-    DB_URL  = os.environ.get("DB_URL", "")  # ⚠️ Required
+    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://nox:nox@nox.0erqdpv.mongodb.net/?retryWrites=true&w=majority
+    ")  # ⚠️ Required
     DB_NAME  = os.environ.get("DB_NAME","SnowEncoderBot") 
 
     # Other Configs 
-    ADMIN = int(os.environ.get("ADMIN", "0")) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '0')) # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN", "5154912723")) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002052189895')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
     START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
 
